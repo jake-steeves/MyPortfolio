8879f29a8878197e539887f5b84407585853da0d
Jake <jsteeves@college.harvard.edu>
2018-04-12 21:36:00 -0400

    basic portfolio implementation and styling

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './style.css'
+
+export default class App extends React.Component {
+  render() {
+    <div>
+      Hello, world!
+    </div>
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import myPic from './croppedpic.jpg';
 import './App.css';
+
+const myDetails = {
+  myName: "Jake Steeves",
+  myUni: "Harvard College",
+  myConc: "Computer Science",
+  myBirthDay: "06/05/1997"
+};
+
+
+const Info = (props) => (
+  <div id="infoHolder">
+    <img src={props.picture} id="myPic"/>
+
+    <div id="detailHolder">
+      <div>{props.details.myName}</div>
+      <div>{props.details.myUni}</div>
+      <div>{props.details.myConc}</div>
+    </div>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
+      <div id="wrapper">
+        <Info details={myDetails} picture={myPic} /> 
+      </div>
+/*
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Testing</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+*/
     );
   }
 }
