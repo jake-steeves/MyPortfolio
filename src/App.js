c0ff49ca21550afdc7e2bc93d708e1c75f680c0d
Jake Steeves <jsteeves@college.harvard.edu>
2018-04-13 00:38:59 -0400

    changed style

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import myPic from './croppedpic.jpg';
+import background from './dark_bg.png';
 import './App.css';
 
 const myDetails = {
   myName: "Jake Steeves",
   myUni: "Harvard College",
   myConc: "Computer Science",
-  myBirthDay: "06/05/1997"
+  myBirthDay: new Date(1997, 6, 5)
 };
 
 
 const Info = (props) => (
   <div id="infoHolder">
-    <img src={props.picture} id="myPic"/>
-
     <div id="detailHolder">
       <div>{props.details.myName}</div>
       <div>{props.details.myUni}</div>
       <div>{props.details.myConc}</div>
+      <div>Age {props.age}</div>
     </div>
   </div>
 );
 
 class App extends Component {
+  static getAge() {
+    let msDiff = Date.now() - myDetails.myBirthDay;
+    let ageDate = new Date(msDiff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
   render() {
     return (
       <div id="wrapper">
-        <Info details={myDetails} picture={myPic} /> 
+        <Info details={myDetails} age={App.getAge()} /> 
+        <img src={myPic} id="myPic"/>
       </div>
 /*
       <div className="App">
