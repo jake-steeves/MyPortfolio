79ae167b5817fbc4c36aa74598bd23b7d6b12b9b
Jake <jsteeves@college.harvard.edu>
2018-08-05 23:43:54 -0400

    Connected created theme file to app and header background color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
 import MyInfo from './components/MyInfo'
 import Portfolio from './components/Portfolio'
 import { AppWrapper, Header, MyPicture, Navbar } from './styles'
@@ -7,14 +9,16 @@ import myPic from './images/myPicture.jpg'
 class App extends Component {
   render() {
     return (
-      <AppWrapper>
-        <Header>
-          <Navbar />
-          <MyInfo />
-        </Header>
-        <MyPicture src={myPic} className="myPic" alt="Jake Steeves" />
-        <Portfolio />
-      </AppWrapper>
+      <ThemeProvider theme={theme}>
+        <AppWrapper>
+          <Header>
+            <Navbar />
+            <MyInfo />
+          </Header>
+          <MyPicture src={myPic} className="myPic" alt="Jake Steeves" />
+          <Portfolio />
+        </AppWrapper>
+      </ThemeProvider>
     )
   }
 }
