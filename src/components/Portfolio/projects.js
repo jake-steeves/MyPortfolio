77f916b5062e822d81f45ea3c8ed2a51a9ed6133
Jake <jsteeves@college.harvard.edu>
2018-09-07 19:23:04 -0400

    Restyle general Portfolio component

--- src/components/Portfolio/projects.js
+++ src/components/Portfolio/projects.js
@@ -5,7 +5,7 @@
         blurb: for tile preview,
         description: for expanded tile,
         image: image for tile,
-        languages: languages used for the project,
+        technologies: technologies used for the project,
         codeLink: link to project code,
         demoLink: link to project demo
       }
@@ -15,33 +15,30 @@ const projects = [
   {
     key: 0,
     title: 'RH Playlist',
-    blurb:
-      'I created this web site for Record Hospital DJs to digitally log songs they play from the playlist of new music. It also automates the creation and publication of weekly top twenty charts.',
+    blurb: 'RH Playlist',
     description: 'TODO: add a description',
     image: 'stage.jpg',
-    languages: ['Python', 'JavaScript', 'HTML', 'CSS', 'MySQL'],
+    technologies: ['Python', 'JavaScript', 'HTML', 'CSS', 'MySQL'],
     codeLink: 'https://www.google.com',
     demoLink: 'https://rhplaylist.com'
   },
   {
     key: 1,
     title: 'CS263 Final Project',
-    blurb:
-      'Final project for CS263: Systems Security. We created a protocol for a botnet controller to communicate over Twitter replies via scripts encoded in publically posted images.',
+    blurb: 'Public Image Botnet Controller',
     description: 'TODO: add a description',
     image: 'bots.jpg',
-    languages: ['Python', 'shell'],
+    technologies: ['Python', 'shell'],
     codeLink: 'https://github.com/jake-steeves/cs263-final-project',
     demoLink: ''
   },
   {
     key: 2,
     title: 'GroupMe Bots',
-    blurb:
-      "Using the GroupMe API and Google Cloud Platform's App Engine, I set up bots to perform functions in a personal group chat. A PeeWee Hermandroid stores a list of secret words, and another bot sends pictures of smiley faces that contain text corresponding to certain phrases.",
+    blurb: 'GroupMe Bots',
     description: 'TODO: add a description',
     image: 'groupme.png',
-    languages: ['Python', 'shell'],
+    technologies: ['Python', 'shell'],
     codeLink: '',
     demoLink: ''
   }
