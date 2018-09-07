69a99924844c46b2f5052c6d8e19c5fd86d69e70
Jake <jsteeves@college.harvard.edu>
2018-09-07 19:23:04 -0400

    Restyle general Portfolio component

--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -1,11 +1,27 @@
 import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+export const PortfolioHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+`
+
+export const PortfolioTitle = styled.div`
+  color: ${themeGet('colors.secondary')};
+
+  font-size: ${themeGet('sizes.sectionTitle')};
+  font-weight: bold;
+
+  margin: 20px 0 10px;
+`
 
 export const TileWrapper = styled.div`
   display: flex;
   flex-flow: row wrap;
 
   justify-content: center;
-  padding: 20px 0;
 
   width: 100%;
 `
