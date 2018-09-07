717bc7380f1e1e665e6a2a10284a251dd40cc016
Jake <jsteeves@college.harvard.edu>
2018-09-07 17:10:37 -0400

    Restyle header

--- src/styles/index.js
+++ src/styles/index.js
@@ -10,22 +10,23 @@ export const AppWrapper = styled.div`
 export const MyPicture = styled.img`
   background: #353131;
   border-radius: 50%;
-  box-shadow: 0px 1px 2px 3px #353131;
-
-  margin: -7.5% 0 0 10%;
+  border: 2px solid ${themeGet('colors.primary')};
 
   height: auto;
-  width: 15%;
+  width: 290px;
 `
 
 export const Header = styled.div`
   background-color: ${themeGet('colors.secondary')};
-  box-shadow: 0px 2px 1px 2px #0d0c0c;
 
   display: flex;
   flex-direction: column;
 
-  height: 35vh;
+  overflow: hidden;
+  align-items: center;
+  padding: 50px 0 0 0;
+
+  height: 500px;
   width: 100%;
 `
 
