14617aa5a19e8b7c609d16768e46101cee9c54e2
Jake Steeves <jsteeves@college.harvard.edu>
2018-09-09 05:36:42 -0400

    Fix navbar centering and add icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,40 +1,60 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { withBreakpoints } from 'react-breakpoints'
 import { goToAnchor } from 'react-scrollable-anchor'
 
-import { NavbarContainer, MobileNav, DesktopNav, NavIcon } from './styles'
+import {
+  NavbarContainer,
+  NavbarOptions,
+  MobileNav,
+  DesktopNav,
+  NavSectionTitle,
+  NavIcon
+} from './styles'
 
-const sections = ['About Me', 'Projects']
+const sections = ['Home', 'About Me', 'Projects']
 const sectionAnchors = {
+  Home: 'home',
   'About Me': 'about',
   Projects: 'projects'
 }
 
 const sectionIcons = {
+  Home: 'home',
   'About Me': 'user',
   Projects: 'folder-open'
 }
 
-class Navbar extends Component {
-  render() {
-    const { breakpoints, currentBreakpoint } = this.props
-    return (
-      <NavbarContainer>
-        {sections.map(section => {
-          const anchor = sectionAnchors[section]
-          return breakpoints[currentBreakpoint] <= breakpoints.mobile ? (
-            <MobileNav key={section} onClick={() => goToAnchor(anchor)}>
-              <NavIcon icon={sectionIcons[section]} />
-            </MobileNav>
-          ) : (
-            <DesktopNav key={section} onClick={() => goToAnchor(anchor)}>
-              {section}
-            </DesktopNav>
-          )
-        })}
-      </NavbarContainer>
-    )
-  }
+const NavSection = props => {
+  const {
+    section,
+    breakpointInfo: { breakpoints, currentBreakpoint }
+  } = props
+  const anchor = sectionAnchors[section]
+
+  return breakpoints[currentBreakpoint] <= breakpoints.mobile ? (
+    <MobileNav key={section} onClick={() => goToAnchor(anchor)}>
+      <NavIcon icon={sectionIcons[section]} />
+    </MobileNav>
+  ) : (
+    <DesktopNav key={section} onClick={() => goToAnchor(anchor)}>
+      <NavIcon icon={sectionIcons[section]} />
+      <NavSectionTitle>{section}</NavSectionTitle>
+    </DesktopNav>
+  )
 }
+
+const Navbar = props => (
+  <NavbarContainer>
+    <NavbarOptions>
+      {sections.map(section => (
+        <NavSection
+          key={section}
+          section={section}
+          breakpointInfo={{ ...props }}
+        />
+      ))}
+    </NavbarOptions>
+  </NavbarContainer>
+)
 
 export default withBreakpoints(Navbar)
