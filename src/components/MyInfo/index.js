0382bc6e02da7253f8da74ed7973e662be9c5bab
Jake Steeves <jsteeves@college.harvard.edu>
2018-09-09 04:44:10 -0400

    Create icons for outside urls

--- src/components/MyInfo/index.js
+++ src/components/MyInfo/index.js
@@ -7,17 +7,39 @@ import {
   Employer,
   EmployerLink,
   Email,
-  Phone
+  Phone,
+  OtherLinks,
+  Link,
+  LinkIcon
 } from './styles'
 
 const details = {
   name: 'Jake Steeves',
   profession: 'Chief Technical Officer for DEV Cloud',
   employer: 'HSA DEV',
-  employerLink: 'https://www.dev.hsa.net',
   location: 'Cambridge, MA',
   email: 'jsteeves@college.harvard.edu',
-  phone: '(508) 446-7586'
+  phone: '(508) 446-7586',
+  employerUrl: 'https://www.dev.hsa.net',
+  linkedinUrl: 'https://www.linkedin.com/in/jake-steeves-636bb611b/',
+  githubUrl: 'https://github.com/jake-steeves'
+}
+
+const OtherPages = props => {
+  const { employerUrl, linkedinUrl, githubUrl } = props.urls
+  return (
+    <OtherLinks>
+      <Link href={employerUrl}>
+        <LinkIcon icon="building" />
+      </Link>
+      <Link href={linkedinUrl}>
+        <LinkIcon icon={['fab', 'linkedin']} />
+      </Link>
+      <Link href={githubUrl}>
+        <LinkIcon icon={['fab', 'github']} />
+      </Link>
+    </OtherLinks>
+  )
 }
 
 const MyInfo = () => {
@@ -26,20 +48,23 @@ const MyInfo = () => {
     profession,
     location,
     employer,
-    employerLink,
     email,
-    phone
+    phone,
+    employerUrl,
+    linkedinUrl,
+    githubUrl
   } = details
   return (
     <DetailHolder>
       <Name>{name}</Name>
       <Profession>{profession}</Profession>
       <Employer>
-        at <EmployerLink href={employerLink}>{employer}</EmployerLink> in{' '}
+        at <EmployerLink href={employerUrl}>{employer}</EmployerLink> in{' '}
         {location}
       </Employer>
       <Email>{email}</Email>
       <Phone>{phone}</Phone>
+      <OtherPages urls={{ employerUrl, linkedinUrl, githubUrl }} />
     </DetailHolder>
   )
 }
--- src/components/MyInfo/styles.js
+++ src/components/MyInfo/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { themeGet } from 'styled-system'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const DetailHolder = styled.div`
   color: #edf3f8;
@@ -45,5 +46,25 @@ export const Email = styled.div`
 
 export const Phone = styled.div`
   font-size: 16px;
-  margin-bottom: 4px;
+  margin-bottom: 12px;
+`
+
+export const OtherLinks = styled.div`
+  display: flex;
+
+  justify-content: center;
+`
+
+export const LinkIcon = styled(FontAwesomeIcon)`
+  cursor: pointer;
+
+  font-size: 24px;
+
+  margin: 0px 10px 10px;
+
+  color: ${themeGet('colors.primary')};
+`
+
+export const Link = styled.a.attrs({ target: '_blank' })`
+  text-decoration: none;
 `
